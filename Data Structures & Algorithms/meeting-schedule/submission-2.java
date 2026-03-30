0f316c5472acb9c4509f458ef99774be3bdb2a11
Vagdevi Junnuri <vagdevijunnuri611@gmail.com>
2026-03-30 01:14:57 -0700

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.java
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if(intervals==null || intervals.size()==0) return true;
+        Collections.sort(intervals, (a,b)->Integer.compare(a.start,b.start));
+        int prevEnd = intervals.get(0).end;
+        for(int i=1;i<intervals.size();i++){
+            if(intervals.get(i).start>=prevEnd) {
+                prevEnd = intervals.get(i).end;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
