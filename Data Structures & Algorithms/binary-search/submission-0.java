fa4692ef96ec7f1470456d176a26c64aed028a00
Vagdevi Junnuri <vagdevijunnuri611@gmail.com>
2026-05-25 17:37:51 -0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0, r = n-1;
+        while(l<=r){
+            int m = l+((r-l)/2);
+            if(nums[m]>target){
+                r = m-1;
+            }else if(nums[m]<target){
+                l = m+1;
+            }else{
+                return m;
+            }
+        }
+        return -1;
+    }
+}
